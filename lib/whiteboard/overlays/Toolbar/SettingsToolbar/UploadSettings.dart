6bbc7324a16818ba66667c9e9fb2a70ed575e547
Yonggan <yonggan@oblivioncoding.pro>
2021-07-09 12:27:56 +0200

    Fix image scaling

--- lib/whiteboard/overlays/Toolbar/SettingsToolbar/UploadSettings.dart
+++ lib/whiteboard/overlays/Toolbar/SettingsToolbar/UploadSettings.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:fluffy_board/utils/own_icons_icons.dart';
 import 'package:fluffy_board/whiteboard/InfiniteCanvas.dart';
 import 'package:fluffy_board/whiteboard/Websocket/WebsocketConnection.dart';
 import 'package:fluffy_board/whiteboard/Websocket/WebsocketTypes.dart';
 import 'package:flutter/material.dart';
-import 'dart:ui';
+import 'dart:ui' as ui;
+import 'package:image/image.dart' as IMG;
 
 import '../../../DrawPoint.dart';
 import '../../../WhiteboardView.dart';
@@ -20,19 +22,22 @@ class UploadSettings extends StatefulWidget {
   WebsocketConnection? websocketConnection;
   OnSaveOfflineWhiteboard onSaveOfflineWhiteboard;
 
-  UploadSettings({required this.selectedUpload,
-  required this.toolbarOptions,
-  required this.onChangedToolbarOptions,
-  required this.uploads,
-  required this.onUploadsChange,
-  required this.websocketConnection,
-  required this.onSaveOfflineWhiteboard});
+  UploadSettings(
+      {required this.selectedUpload,
+      required this.toolbarOptions,
+      required this.onChangedToolbarOptions,
+      required this.uploads,
+      required this.onUploadsChange,
+      required this.websocketConnection,
+      required this.onSaveOfflineWhiteboard});
 
   @override
   _UploadSettingsState createState() => _UploadSettingsState();
 }
 
 class _UploadSettingsState extends State<UploadSettings> {
+  double uploadSize = 1;
+
   @override
   @override
   Widget build(BuildContext context) {
@@ -48,17 +53,49 @@ class _UploadSettingsState extends State<UploadSettings> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              OutlinedButton(onPressed: () {
-                setState(() {
-                  widget.uploads.remove(widget.selectedUpload!);
-                  sendUploadDelete(widget.selectedUpload!);
-                  widget.onUploadsChange(widget.uploads);
-                  widget.onSaveOfflineWhiteboard();
-                });
-              }, child: Padding(
-                padding: const EdgeInsets.all(14.0),
-                child: Icon(Icons.delete),
-              ))
+              RotatedBox(
+                quarterTurns: -1,
+                child: Slider.adaptive(
+                  value: uploadSize,
+                  onChanged: (value) async {
+                    setState(() {
+                      uploadSize = value;
+                    });
+                  },
+                  onChangeEnd: (value) async {
+                    int index = widget.uploads.indexOf(widget.selectedUpload!);
+                    widget.selectedUpload!.uint8List =
+                        resizeImage(widget.selectedUpload!.uint8List, value);
+                    final ui.Codec codec = await PaintingBinding.instance!
+                        .instantiateImageCodec(
+                            widget.selectedUpload!.uint8List);
+                    final ui.FrameInfo frameInfo = await codec.getNextFrame();
+
+                    widget.selectedUpload!.image = frameInfo.image;
+                    widget.uploads[index] = widget.selectedUpload!;
+                    widget.onUploadsChange(widget.uploads);
+                    widget.onSaveOfflineWhiteboard();
+                    setState(() {
+                      uploadSize = 1;
+                    });
+                  },
+                  min: 0.01,
+                  max: 2,
+                ),
+              ),
+              OutlinedButton(
+                  onPressed: () {
+                    setState(() {
+                      widget.uploads.remove(widget.selectedUpload!);
+                      sendUploadDelete(widget.selectedUpload!);
+                      widget.onUploadsChange(widget.uploads);
+                      widget.onSaveOfflineWhiteboard();
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(14.0),
+                    child: Icon(Icons.delete),
+                  ))
             ],
           ),
         ),
@@ -66,11 +103,25 @@ class _UploadSettingsState extends State<UploadSettings> {
     );
   }
 
-  sendUploadDelete(Upload newUpload){
+  sendUploadDelete(Upload newUpload) {
     String data = jsonEncode(WSUploadDelete(
       newUpload.uuid,
     ));
     if (widget.websocketConnection != null)
       widget.websocketConnection!.sendDataToChannel("upload-delete#", data);
+  }
+
+  Uint8List resizeImage(Uint8List data, double scaleFactor) {
+    Uint8List resizedData = data;
+    IMG.Image? img = IMG.decodeImage(data);
+    print(scaleFactor);
+    print(img!.width.toString());
+    print(img.height.toString());
+    print(img.height.toString() * scaleFactor.toInt());
+    IMG.Image resized = IMG.copyResize(img,
+        width: (img.width * scaleFactor).toInt(),
+        height: (img.height * scaleFactor).toInt());
+    resizedData = Uint8List.fromList(IMG.encodePng(resized));
+    return resizedData;
   }
 }
